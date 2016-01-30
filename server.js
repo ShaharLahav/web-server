8b44d21382ec1fe60534296ee68be2e445f1e2e9
Shahar Lahav <lahav.shahar@gmail.com>
2016-01-30 21:24:40 +0200

    Initialize repo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middlewware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log(new Date().toString() + 'Request: ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+ app.use(middlewware.logger);
+
+app.get('/about' ,middlewware.requireAuthentication , function (req, res) {
+	res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function () {
+	console.log('Express server Started on port: ' + PORT);
+});
